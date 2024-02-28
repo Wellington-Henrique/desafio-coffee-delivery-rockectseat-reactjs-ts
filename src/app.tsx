51fc3f035420210bda7de9a77cfe49d170423e37
Wellington Henrique <wellingtonhlc@gmail.com>
2024-02-28 14:48:06 -0300

    Configuring inital route

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,17 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/globals"
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./Router"
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle/>
-      <h1>Coffee Delivery</h1>
+
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
