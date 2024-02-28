31cfa5902ba63f1485b56ce5c70dc2dc180bad2e
Wellington Henrique <wellingtonhlc@gmail.com>
2024-02-28 14:28:39 -0300

    Configuring the project structure

---
+++ src/@types/styles.d.ts
@@ -0,0 +1,10 @@
+// styles.d.ts, onde d significa que somente códigos de definição do typescript
+
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
